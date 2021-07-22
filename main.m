a6a2ccf38170efcceb2d29c241ce90fc938c60a1
Kihoon-Shin <82350419+Kihoon-Shin@users.noreply.github.com>
2021-07-22 17:54:14 +0900

    adding main func

--- main.m
+++ main.m
@@ -1,54 +1,23 @@
-# drone
-# Strategy
- 2021 미니드론경진대회 전략으로 1단계에서는 빠른 시간내에 통과를 목적으로 하며, 2단계와 3단계에서는 정확성을 중시하며 통과를 최우선 순위로 하고, 빠른 시간은 두번째 우선순위로 삼는다. 또한 우리팀은 정확성과 속도의 향상을 위해서 image processing toolbox에서 제공되는 여러 함수들을 활용하여서 이미지 처리를 수행하였다. (rgb2hsv, imcrop, regionprops함수)  또한 장애물까지의 거리를 측정할 때 거리에 따른 1,2,3단계별로 링의 직경을 여러번 측정하여 드론이 현재 위치에서 링을 통과하기 위한 거리(Input)를 계산하여 정확성을 높였다. 코드 작성 후에 실제로 장애물을 설치하여 코드가 제대로 동작하는지 확인하였으며, 여러번 시행착오를 통해 알고리즘을 개선하였다.
-# Algorithm
-> 1. 드론 객체 선언 및 takeoff
+clear, clc;
 
-> 2. 링의 구멍을 이미지 처리하여 구멍을 찾아낸다. 구멍에서 많이 벗어난 경우 상하 좌우로 이동한다.
-
-> 3. 이미지 속에서 링의 중점을 구하고 상하좌우로 조금씩 이동하여 드론을 중점에 맞춘다.
-
-> 4. 링의 구멍의 장축을 측정하여 드론과의 거리를 계산한 뒤 링을 통과킨다. 
-> (드론과 장애물까지의 거리에 따라 링의 장축의 길이가 달라지므로 이를 이용하여 거리를 측정하였다.)
-
-> 5. 표식을 인식하여 빨간색이면 회전, 보라색이면 착지를 한다
-
-> 6. 착지하기 전까지 **2**단계로 돌아가 반복한다
-
-
-# Source Code
-Source Code를 알고리즘 단계별로 기술하였다. 상세한 설명은 주석을 통하여 확인할 수 있다.
-
-1. 변수 설정 및 takeoff
-```matlab
-%초기화
-clear,clc ;
-
-%변수 설정
-drone=ryze();   
+%드론 설정 및 takeoff
+drone=ryze()
 cam=camera(drone);
-originCenter=[480 170; 480 170; 480 170]; 
+
+originCenter=[480 168];
 count=0;
-max=0;
+max_r=0;
 none=0;
 takeoff(drone);
-```
-2. 구멍 찾기
 
-```matlab
-%level별로 반복문 설정
 for level = 1:3
-    %% finding hole
     
-    %1단계에서는 시작시에 좌우에 대한 중점이 맞춰져 있음
-    %1단계에서는 중점을 찾을 필요가 없으므로 높이에 대한 중점만 맞춰줌
     if level == 1
         moveup(drone, 'distance', 0.25)
     end
     
+    %% finding hole
     while 1
-        
-        %1단계의 경우 구멍을 찾지 않음
         if level == 1
             break;
         end
@@ -80,7 +49,7 @@ for level = 1:3
         end
         
         %변환한 이미지의 픽셀 수가 1000이상이면 구멍을 인식했다고 파악
-        %10000이하이면 상승하여 전 과정을 다시 반복
+        %1000이하이면 상승하여 전 과정을 다시 반복
         if sum(bw2,'all')>10000
             disp('find hole!');
             break;
@@ -90,7 +59,6 @@ for level = 1:3
             diff_lr = sum(imcrop(blue,[0 0 480 720]),'all') - sum(imcrop(blue,[480 0 480 720]),'all');
             diff_ud = sum(imcrop(blue,[0 0 960 360]),'all') - sum(imcrop(blue,[0 360 960 360]),'all');
             
-            %장애물에 대한 이미지의 좌우 차이값이 30000이상이면 좌우로 이동
             if diff_lr > 30000
                 moveleft(drone,'distance',0.5,'speed',1);
                 disp('finding hole_move_left 0.5m');
@@ -100,7 +68,6 @@ for level = 1:3
                 disp('finding hole_move_right 0.4m');
             end
             
-            %장애물에 대한 이미지의 상하 차이값이 10000이상이면 상하로 이동
             if diff_ud > 10000
                 moveup(drone,'distance',0.3,'speed',1);
                 disp('finding hole_move_up_0.3m');
@@ -109,8 +76,7 @@ for level = 1:3
                 disp('finding hole_move_down_0.2m');
             end
             
-            %장애물이 카메라에 잡히지 않을 경우의 예외처리
-            %상하좌우로 이동하며 장애물 인식
+            % 사진 찍어서 noise 및 diff_ud 확인
             if sum(blue,'all') < 3000
                 if none==0
                     if diff_ud >= 0
@@ -121,12 +87,10 @@ for level = 1:3
                         disp('Cannot find barrier_moving_down');
                     end
                     
-                    %처음에는 오른쪽으로 1m 이동
                     moveright(drone,'distance',1,'speed',1);
                     disp('Cannot find barrier_moving_right_1m');
                     none = none+1;
                     
-                    %오른쪽에 장애물 없을 시 처음 위치기준 왼쪽으로 1.1m 이동
                 elseif none==1
                     moveleft(drone,'distance',2.1,'speed',1);
                     disp('Cannot find barrier_moving_left_2.1m');
@@ -135,18 +99,13 @@ for level = 1:3
             end
         end
     end
-```
-
-3. 장애물의 중점 찾기
-```matlab
- %% find center point
     
-    %같은 동작을 반복하는 loop에 빠질 경우 예외처리를 위한 변수 설정
+    
+    %% find center point
+    
     count_l = 0; count_r = 0; count_u = 0; count_d = 0; %*
     
     while 1
-        
-        %1단계의 경우 장애물의 중점을 찾지 않음
         if level == 1
             break;
         end
@@ -197,7 +156,7 @@ for level = 1:3
         ct_diff_lr = firstCenter(1,1)-originCenter(1,1);
         ct_diff_ud = firstCenter(1,2)-originCenter(1,2);
         
-        % x축에 대한 중점 찾기
+        % x 중점 찾기
         if ct_diff_lr >= 40 && ct_diff_lr <= 80
             moveright(drone,'Distance',0.2,'speed',1);
             disp('finding center point_move right_0.2m');
@@ -217,10 +176,10 @@ for level = 1:3
             moveleft(drone,'Distance',0.3,'speed',1);
             disp('finding center point_move left_0.3m');
             count_l = count_l + 1; %*
-            
+             
         end
         
-        % y축에 대한 중점찾기
+        % y 중점찾기
         if ct_diff_ud >= 30 && ct_diff_ud <= 75
             movedown(drone,'Distance',0.2,'speed',1);
             disp('finding center point_move down_0.2m');
@@ -242,29 +201,25 @@ for level = 1:3
             count_u = count_u + 1; %*
         end
         
-        %max 함수를 사용하기 위해 벡터 정의
+        
         tmp = [count_l, count_r, count_u, count_d]; %*
         
-        %같은 동작을 반복하는 loop에 빠질 경우 예외처리
-        %장애물이 가까워서 계속 같은 행동을 반복하는 loop에 갇힐 시 빠져나오기 위해 0.5m 뒤로 이동
-        if max(tmp) >= 6 %*
-            disp('a drone is in loop. moveback_0.3m'); %*
+        % 장애물이 가까워서 계속 같은 행동을 반복하는 loop에 갇힐 시 빠져나오기 위해 0.5m 뒤로 이동
+        if max(tmp) >= 7 %*
+            disp('a drone is in loop. moveback_0.5m'); %*
             moveback(drone, 'distance', 0.5); %*
             count_l=0; count_r=0; count_u=0; count_d=0; %*
         end %*
         
-        %오차범위 내에 있으면 반복문 탈출(드론이 장애물 중점 위치로 이동 완료)
+        %오차범위 내에 있으면 반복문 탈출
         if ct_diff_ud < 30 && ct_diff_ud > -30 && ct_diff_lr < 40 && ct_diff_lr > -40
             disp('find center point!');
             count_l=0; count_r=0; count_u=0; count_d=0; %*
             break;
         end
     end
-```
-4. 장애물까지의 거리 측정 및 이동
-```matlab
- %% measure distance from barrier and moving
     
+    %% find distance to barrier and moving
     %파란색에 대한 HSV값 설정 및 이진화
     frame = snapshot(cam);
     hsv = rgb2hsv(frame);
@@ -295,12 +250,6 @@ for level = 1:3
     stats = regionprops('table', bw2, 'MajorAxisLength');
     long_rad = max(stats.MajorAxisLength);
     
-    %장애물로 부터 드론까지의 거리를 구하기 위한 알고리즘
-    %장매물까지의 거리와 장축의 길이를 1대1로 대응하여 위치 추정
-    
-    %md의 첫번째 행은 1단계(지름 78cm) 1m부터 3m까지 0.3m간격으로 측정시 장축의 길이
-    %md의 두번째 행은 2단계(지름 57cm) 1m부터 3m까지 0.3m간격으로 측정시 장축의 길이
-    %md의 세번째 행은 3단계(지름 50cm) 1m부터 3m까지 0.3m간격으로 측정시 장축의 길이
     md = [860 670 530 440 370 315 285; 550 450 370 320 265 230 200; 460 380 310 255 225 198 175];
     
     %각 단게에서 long_rad의 값에 따라서 거리 추정
@@ -359,11 +308,8 @@ for level = 1:3
         long_rad
         
     end
-```
-5. 빨간색 점 및 보라색 점 찾은 후 회전 혹은 착지
-```matlab
- %% detecting sign
     
+    %% detecting sign
     % 1,2 단계일 때 수행
     if level==1 || level==2
         %빨간점 찾기
@@ -420,9 +366,8 @@ for level = 1:3
                 count=1;
             end
         end
-    end
+    end    
 end
 
 clear drone;
 clear cam;
-```
